1e132dfe6c9f2269aafdf9d4148057b8dad73e2f
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2025-08-14 15:03:08 +0200

    commit

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,6 +1,7 @@
 import type {Todo} from "../Todo.ts";
 import axios from "axios";
 import {useState} from "react";
+import type {TodoStatus} from "../TodoStatus.ts";
 
 type Props = {
     todo: Todo,
@@ -11,7 +12,7 @@ export default function TodoCard(props: Props) {
 
     const [description, setDescription] = useState(props.todo.description);
 
-    function deleteThisTodo(){
+    function deleteThisTodo() {
         axios.delete("/api/todo/" + props.todo.id)
             .then(props.onTodoItemChange)
     }
@@ -19,18 +20,41 @@ export default function TodoCard(props: Props) {
     function changeText(event: React.ChangeEvent<HTMLInputElement>) {
         const newDescription = event.target.value;
         setDescription(newDescription)
-        axios.put("/api/todo/"+props.todo.id, {
+        axios.put("/api/todo/" + props.todo.id, {
             id: props.todo.id,
             description: newDescription,
             status: props.todo.status
         } as Todo)
     }
 
+    function move(targetStatus: TodoStatus) {
+        axios.put("/api/todo/" + props.todo.id, {
+            ...props.todo,
+            status: targetStatus,
+        } as Todo)
+            .then(props.onTodoItemChange)
+    }
 
     return (
         <div className="todo-card">
             <input value={description} onInput={changeText}/>
+            {
+                (props.todo.status === "OPEN")
+                    ? <div></div>
+                    : ((props.todo.status === "IN_PROGRESS")
+                        ? <button onClick={() => move("OPEN")}>⬅️</button>
+                        : <button onClick={() => move("IN_PROGRESS")}>⬅️</button>)
+            }
             <button onClick={deleteThisTodo}>🗑️</button>
+            {
+                (props.todo.status === "DONE")
+                    ? <div></div>
+                    : (
+                        (props.todo.status === "OPEN")
+                            ? <button onClick={() => move("IN_PROGRESS")}>➡️</button>
+                            : <button onClick={() => move("DONE")}>➡️</button>
+                    )
+            }
         </div>
     );
 }
